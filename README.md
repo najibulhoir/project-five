1463f70f8112a4f11f39479e76e2778d53bff573
najibulhoir <najibulhoir98@gmail.com>
2018-11-15 20:25:34 +0700

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+# project-five
+# project-five
+# project-five
---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
+import { FooterComponent } from './footer/footer.component';
+import { DropdownDirective } from "./shared/dropdown.directive";
+import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { RecipeStartComponent } from './recipe/recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe/recipe-edit/recipe-edit.component';
+
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      HeaderComponent,
+      ShoppingListComponent,
+      ShoppingEditComponent,
+      RecipeComponent,
+      RecipeListComponent,
+      RecipeDetailComponent,
+      RecipeItemComponent,
+      FooterComponent,
+      DropdownDirective,
+      RecipeStartComponent,
+      RecipeEditComponent
+   ],
+   imports: [
+      BrowserModule,
+      FormsModule,
+      HttpModule,
+      AppRoutingModule
+   ],
+   providers: [ShoppingListService],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
